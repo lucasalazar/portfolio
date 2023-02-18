29cea1c03986cc7a6b3095c745bf9aef2e3f7d66
Luca Salazar <omelhordobrasil@protonmail.com>
2023-02-18 16:18:34 -0300

    teste2

--- components/molecules/ProjectGroup.vue
+++ components/molecules/ProjectGroup.vue
@@ -16,43 +16,43 @@
         link="https://scratch.mit.edu/projects/501917857/"
       />
       <ProjectCard
-        :img="require('/projeto2.png')"
+        img="'/projeto2.png'"
         title="Portal de Filmes - V1"
         :text="$t('18')"
         link="https://portal-de-filmes-xi.vercel.app/"
       />
       <ProjectCard
-        :img="require('/projeto3.png')"
+        img="'/projeto3.png'"
         title="Hot Finger Genius - Scratch"
         :text="$t('19')"
         link="https://hot-finger-genius.vercel.app/"
       />
       <ProjectCard
-        :img="require('/projeto4.png')"
+        img="'/projeto4.png'"
         title="Verificador de Site - V1"
         :text="$t('20')"
         link="https://verificador-de-sites.vercel.app/"
       />
       <ProjectCard
-        :img="require('/projeto5.png')"
+        img="'/projeto5.png'"
         title="Portal de Filmes - V2"
         :text="$t('21')"
         link="https://portal-de-filmes-diw.vercel.app/"
       />
       <ProjectCard
-        :img="require('/projeto6.png')"
+        img="'/projeto6.png'"
         title="C_Like"
         :text="$t('22')"
         link="#"
       />
       <ProjectCard
-        :img="require('/projeto7.png')"
+        img="'/projeto7.png'"
         title="Luca Mine_RPG"
         :text="$t('23')"
         link="https://github.com/lucasalazar/Luca-MineRPG"
       />
       <ProjectCard
-        :img="require('/projeto8.png')"
+        img="'/projeto8.png'"
         title="Luca dev.finance$"
         :text="$t('24')"
         link="https://github.com/lucasalazar/luca-dev.finances"
