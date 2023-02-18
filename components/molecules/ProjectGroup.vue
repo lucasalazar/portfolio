bacb98c5feee2b17328eedb1cd9c811d58408380
Luca Salazar <omelhordobrasil@protonmail.com>
2023-02-18 16:39:36 -0300

    FIX: image import fix

--- components/molecules/ProjectGroup.vue
+++ components/molecules/ProjectGroup.vue
@@ -10,49 +10,49 @@
       "
     >
       <ProjectCard
-        img="'/projeto1.webp'"
+        :img="require('/projeto1.webp')"
         title="Tower defense - Scratch"
         :text="$t('25')"
         link="https://scratch.mit.edu/projects/501917857/"
       />
       <ProjectCard
-        img="'/projeto2.webp'"
+        :img="require('/projeto2.webp')"
         title="Portal de Filmes - V1"
         :text="$t('18')"
         link="https://portal-de-filmes-xi.vercel.app/"
       />
       <ProjectCard
-        img="'/projeto3.webp'"
+        :img="require('/projeto3.webp')"
         title="Hot Finger Genius - Scratch"
         :text="$t('19')"
         link="https://hot-finger-genius.vercel.app/"
       />
       <ProjectCard
-        img="'/projeto4.webp'"
+        :img="require('/projeto4.webp')"
         title="Verificador de Site - V1"
         :text="$t('20')"
         link="https://verificador-de-sites.vercel.app/"
       />
       <ProjectCard
-        img="'/projeto5.webp'"
+        :img="require('/projeto5.webp')"
         title="Portal de Filmes - V2"
         :text="$t('21')"
         link="https://portal-de-filmes-diw.vercel.app/"
       />
       <ProjectCard
-        img="'/projeto6.webp'"
+        :img="require('/projeto6.webp')"
         title="C_Like"
         :text="$t('22')"
         link="#"
       />
       <ProjectCard
-        img="'/projeto7.webp'"
+        :img="require('/projeto7.webp')"
         title="Luca Mine_RPG"
         :text="$t('23')"
         link="https://github.com/lucasalazar/Luca-MineRPG"
       />
       <ProjectCard
-        img="'/projeto8.webp'"
+        :img="require('/projeto8.webp')"
         title="Luca dev.finance$"
         :text="$t('24')"
         link="https://github.com/lucasalazar/luca-dev.finances"
